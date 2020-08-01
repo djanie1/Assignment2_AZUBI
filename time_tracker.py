98a016db3027a6d4334b13098c8254c722c1cedc
djanie <kotey.seth11@gmail.com>
2020-08-01 17:31:25 +0000

    Update time_tracker.py

--- time_tracker.py
+++ time_tracker.py
@@ -1,91 +1,154 @@
 # Write a program that would help Nana track his money.
 
+from tkinter import *
+from datetime import timedelta
+import datetime
 
-from datetime import datetime
 
+#import pandas as pd
 
+#function to remove white spaces from project name to be used as the excel file name
 def remove(string): 
 	return string.replace(" ", "")     #removes spaces in project name to store output file as "projectname.xlsx" without spaces
 
-
-Project_name = input ('Enter name of project:')
-
-
-print("\n")
-print ("START DATE DETAILS")
-dt = datetime.now()
-Start_year = dt.year
-Start_month = dt.month
-Start_day = dt.day
-Start_hour = dt.hour
-Start_minute = dt.minute
-
-print ('The start date is today:', Start_year,'-', Start_month,'-', Start_day)
-print ('The start time is now:', Start_hour,':', Start_minute)
-print("\n")
+window = Tk()
+#window.title("TIME TRACKER")
+window.geometry('285x480')
+window.configure(bg='white')
 
 
 
-print ("FINISH DATE DETAILS")
-Finish_year= input ('Enter finish year of project (eg. 2019):')
-Finish_year= int (Finish_year) # Assign as int because input automatically accepts strings as its data type
-Finish_month= input('Enter finish month of the project (eg.December is represented as 12):')
-Finish_month= int (Finish_month)
-Finish_day= input ('Enter finish day of the month (eg.21):')
-Finish_day= int (Finish_day)
-Finish_time= input ('Enter finish time of the project (eg. 11:00 or 12:30):')
-mylist1= Finish_time.split(':')
-Finish_hour= int(mylist1[0])
-Finish_minute= int(mylist1[1])
+greeting= Label(window, text="Welcome Nana, click Start to start timer", font=("Arial Bold", 10),background='white')
+greeting.grid(column=0, row=1)
+
+begun = Label(window, font=("Arial", 10),background='white')
+begun.grid(column=0, row=3)
+
+#stores start time of project
+def start_time():
+    global start_now
+    start_now = datetime.datetime.now()
+    started=start_now.strftime("Date:%Y-%m-%d  Start time:%H:%M")
+    global started_time
+    started_time=start_now.strftime("%H:%M")
+    begun.configure (text=started)
+    return started_time
+   
+    
+#Start button
+#btn = Button(window, text="START",command=calculate_date_time,background='white')
+#btn.grid(column=0, row=2)
+
+Project = Label(window, text="Enter project name", font=("Arial Bold", 10),background='white')
+Project.grid(column=0, row=4)
+
+#lbl5 = Label(window, text="Amount per hour:", font=("Arial", 10),background='white')
+#lbl5.grid(column=0, row=5)
+
+#variable that holds how much he earns
+name = Entry(window,width=40)
+name.grid(column=0, row=6)
+name.focus()
+
+total_time = Label(window, text="Hours spent:", font=("Arial", 20),background='white')
+total_time.grid(column=0,row=7)
+
+total_amount = Label(window, text="You have earned ($):", font=("Arial", 20),background='white')
+total_amount.grid(column=0,row=9)
+
+#total_time = Label(window, font=("Arial", 10),background='white')
+#total_time.grid(column=0,row=10)
+
+#function that stores your stop time and takes your start time and calculates your earnings
+def end_work():
+    
+    global end_time
+    global hours_worked
+    global total_pay
+
+    stop_time = datetime.datetime.now()
+    
+    end_time=stop_time.strftime("%H:%M")
 
 
+    start_hour=started_time
+    delta1 = timedelta(hours=int(start_hour.split(':')[0]), minutes=int(start_hour.split(':')[1]))
+    hour_start = delta1.total_seconds()/3600
+   
 
-start_date = datetime(Start_year, Start_month, Start_day, Start_hour,Start_minute )
-Finish_date = datetime(Finish_year, Finish_month, Finish_day, Finish_hour,Finish_minute)
+    end_hour=end_time
+    delta2 = timedelta(hours=int(end_hour.split(':')[0]), minutes=int(end_hour.split(':')[1]))
+    hour_end = delta2.total_seconds()/3600
+   
+    
+    #amount he earns
+    Project_name=str(name.get())
+    
+    
+    hours_worked=round(hour_end-hour_start,1)
+    total_pay= hours_worked*float(5)
+    
+    #displays the hours spent working
+    total_time.configure(text=str(hours_worked)+("Hrs"))
+    total_amount.configure(text=("$")+str(total_pay))
+    
+
+    import xlsxwriter 
+    workbook = xlsxwriter.Workbook(remove(Project_name)+".xlsx") 
+    worksheet = workbook.add_worksheet() 
+
+    #Setting Headers
+    worksheet.write('A1', 'ProjectName') 
+    worksheet.write('B1', 'StartDate') 
+    worksheet.write('C1', 'StartTime') 
+    worksheet.write('D1', 'FinishDate') 
+    worksheet.write('E1', 'FinishTime') 
+    worksheet.write('F1', 'HoursSpent') 
+    worksheet.write('G1', 'AmountReceived') 
+
+    worksheet.write('A2', Project_name)
+    g = str(start_now.year)+"-"+str(start_now.month)+"-"+str(start_now.day)
+    worksheet.write('B2',g )  
+    h = str(start_now.hour)+":"+str(start_now.minute)
+    worksheet.write('C2', h) 
+    g1 = str(stop_time.year)+"-"+str(stop_time.month)+"-"+str(stop_time.day)
+    worksheet.write('D2',g1 )  
+    h1 = str(stop_time.hour)+":"+str(stop_time.minute)
+    worksheet.write('E2', h1) 
+    worksheet.write('F2', str(hours_worked)) 
+    worksheet.write('G2', str(total_pay)) 
+    workbook.close()
+
+    
+    
+    
 
 
-difference_between_times=  Finish_date - start_date
-
-hours_of_project=  difference_between_times.total_seconds()/ 60*60
-pay_per_hour = 5
-total_amount_received = hours_of_project * pay_per_hour
-
-currency = "$"
-
-
-
-print("\n")
-print ("DETAILS ON PAYMENT")
-print('Nana spent %f %s on project %s' % (hours_of_project,'hours', Project_name))
-print('Nana will receive %s %f for project %s' % (currency,total_amount_received, Project_name))
+    
+start = Button(
+    text="Start",
+    width=25,
+    height=5,
+    bg="green",
+    fg="white",
+    font=("Arial",15),
+    command=start_time
+)
+start.grid(column=0,row=8)  
 
 
-# Install pip on laptop terminal
-# With pip installed, install xlsxwriter on laptop terminal
-# This helps save information in an Excel file.
+   
+ #stop button 
+finish = Button(
+    text="Finish",
+    width=25,
+    height=5,
+    bg="red",
+    fg="white",
+    font=("Arial",15),
+    command=end_work
+)
+finish.grid(column=0, row=11)
 
-import xlsxwriter 
-workbook = xlsxwriter.Workbook(remove(Project_name)+".xlsx") 
-worksheet = workbook.add_worksheet() 
 
-#Setting Headers
-worksheet.write('A1', 'ProjectName') 
-worksheet.write('B1', 'StartDate') 
-worksheet.write('C1', 'StartTime') 
-worksheet.write('D1', 'FinishDate') 
-worksheet.write('E1', 'FinishTime') 
-worksheet.write('F1', 'HoursSpent') 
-worksheet.write('G1', 'AmountReceived') 
-
-worksheet.write('A2', Project_name)
-g = str(Start_year)+"-"+str(Start_month)+"-"+str(Start_day)
-worksheet.write('B2',g )  
-h = str(Start_hour)+":"+str(Start_minute)
-worksheet.write('C2', h) 
-g1 = str(Finish_year)+"-"+str(Finish_month)+"-"+str(Finish_day)
-worksheet.write('D2',g1 )  
-h1 = str(Finish_hour)+":"+str(Finish_minute)
-worksheet.write('E2', h1) 
-worksheet.write('F2', str(hours_of_project)) 
-worksheet.write('G2', str(total_amount_received)) 
-workbook.close()
+window.mainloop()
